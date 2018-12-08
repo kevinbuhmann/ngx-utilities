8bdcc97b9812e69032736b4f76fe87897fb763c7
Kevin Phelps <KevinPhelps11@gmail.com>
2018-12-08 16:40:49 -0600

    feat (docs): add lazy loaded docs module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DocsHttpRetryComponent } from './docs/docs-http-retry/docs-http-retry.component';
-import { DocsNgxIfElseLoadingComponent } from './docs/docs-ngx-if-else-loading/docs-ngx-if-else-loading.component';
-import { DocsNgxLetComponent } from './docs/docs-ngx-let/docs-ngx-let.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'docs/http-retry', component: DocsHttpRetryComponent },
-  { path: 'docs/ngx-let', component: DocsNgxLetComponent },
-  { path: 'docs/ngx-if-else-loading', component: DocsNgxIfElseLoadingComponent },
+  { path: 'docs', loadChildren: 'app/docs/docs.module#DocsModule' },
   { path: '**', component: NotFoundComponent }
 ];
 
