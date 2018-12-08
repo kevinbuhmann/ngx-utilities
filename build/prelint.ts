3f2fd562545bd6d3890ef89fe89990c2f2aaf130
Kevin Phelps <KevinPhelps11@gmail.com>
2018-12-08 13:49:14 -0600

    chore: exclude coverage folder in prelint

--- build/prelint.ts
+++ build/prelint.ts
@@ -7,7 +7,7 @@ import { bailIfFailures, Failure } from './helpers/utility.helpers';
   const failures: Failure[] = [];
 
   walkDirectory('.', filePath => {
-    if (filePath.includes('.git') === false && filePath.includes('.heroku') === false) {
+    if (['.git', '.heroku', 'coverage'].every(excludedFolder => filePath.includes(excludedFolder) === false)) {
       const fileContents = readFile(filePath);
       failures.push(...checkForEmptyFiles(filePath, fileContents));
       failures.push(...checkForLeadingWhitespace(filePath, fileContents));
