f2c1275d2868ff6d0e8a1fb8c810148b6dc667c3
Kevin Phelps <KevinPhelps11@gmail.com>
2018-12-08 12:58:54 -0600

    refactor (app): update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
+import { commonEnvironment } from './environment.common';
+
+const prodEnvironment: Partial<typeof commonEnvironment> = {
   production: true
 };
+
+export const environment = { ...commonEnvironment, ...prodEnvironment };
