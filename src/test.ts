82f08ed54dde2ff55ab2dfee920c2afe266fb56a
Kevin Phelps <KevinPhelps11@gmail.com>
2018-12-19 21:00:52 -0600

    test: import 'zone.js/dist/zone-testing' for testing

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-// tslint:disable:ordered-imports
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-// tslint:enable:ordered-imports
+import 'zone.js/dist/zone-testing';
 
 import { getTestBed } from '@angular/core/testing';
 import { platformBrowserDynamicTesting, BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
